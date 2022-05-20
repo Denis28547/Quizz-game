dbc6c0e5a2f1feaf4b968e55a9055b810ea81961
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-05-20 13:09:08 +0300

    first_change-try

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Questions() {
-  return fetch("https://opentdb.com/api.php?amount=5")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  // return fetch("https://opentdb.com/api.php?amount=5")
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data));
 }
