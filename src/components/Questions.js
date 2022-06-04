6a2eb26cc5b6c1b5779bdacc928bb2372fa515d3
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-06-04 19:46:23 +0300

    commit

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -73,7 +73,7 @@ export default function Questions() {
     });
   }
 
-  console.log(info);
+  // console.log(info);
 
   // function chooseAnswer(id, selectedQuestion) {
   //   setInfo((prevInfo) => {
